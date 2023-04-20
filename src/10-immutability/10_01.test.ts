8cc343bf92f55d1f3dd4ad390b9477eed320bdaa
tony-developer <anton.serebriakoff@gmail.com>
2023-04-20 13:50:09 -0700

    added NEW tests

--- src/10-immutability/10_01.test.ts
+++ src/10-immutability/10_01.test.ts
@@ -1,23 +1,130 @@
-import {UserType} from "./10_01";
+import {
+    addBooksToUser,
+    makeHairStyle,
+    moveUser, removeBook, updateBook,
+    upgradeUser,
+    UserType,
+    UserWithBooks,
+    UserWithLaptopType
+} from "./10_01";
 
-function makeHairStyle(u:UserType, power: number){
-    const copy = {
-        ...u
-    }
-    copy.hair = u.hair/power
 
-    return copy
-}
 
 test('reference type test',() => {
     let user:UserType={
         name: 'Dimych',
         hair: 32,
         address: {
-            title: 'Minsk'
+            city: 'Minsk',
+            house: 12
         }
     }
-    const cutUser = makeHairStyle(user, 2)
-    expect(cutUser.hair).toBe(16)
+    const awesomeUser = makeHairStyle(user, 2)
+    expect(awesomeUser.hair).toBe(16)
     expect(user.hair).toBe(32)
+    expect(awesomeUser.address).toBe(user.address)
+})
+
+test('change address',() => {
+    let user:UserWithLaptopType={
+        name: 'Dimych',
+        hair: 32,
+        address: {
+            city: 'Minsk',
+            house: 12
+        },
+        laptop: {
+            title:'Zenbook'
+        }
+    }
+    const movedUser = moveUser(user, 'Boston')
+
+    expect(user.address).not.toBe(movedUser.address)
+    expect(user.laptop).toBe(movedUser.laptop)
+    expect(movedUser.address.city).toBe('Boston')
+})
+
+test('upgrade user laptop',()=>{
+    let user:UserWithLaptopType={
+        name: 'Dimych',
+        hair: 32,
+        address: {
+            city: 'Minsk',
+            house: 12
+        },
+        laptop: {
+            title:'Zenbook'
+        }
+    }
+
+    const upgradedUser = upgradeUser(user, 'Macbook')
+
+    expect(upgradedUser).not.toBe(user)
+    expect(upgradedUser.laptop.title).toBe('Macbook')
+    expect(upgradedUser.laptop).not.toBe(user.laptop)
+})
+
+test('add new books to user',()=>{
+    let user:UserWithBooks={
+        name: 'Dimych',
+        hair: 32,
+        address: {
+            city: 'Minsk',
+            house: 12
+        },
+        laptop: {
+            title:'Zenbook'
+        },
+        books: ['css', 'html','js']
+    }
+
+    const userCopy = addBooksToUser(user,'ts')
+
+    expect(userCopy).not.toBe(user)
+    expect(userCopy.books[3]).toBe('ts')
+    expect(userCopy.books).not.toBe(user.books)
+    expect(user.books.length).toBe(3)
+})
+
+test('update books to user',()=>{
+    let user:UserWithBooks={
+        name: 'Dimych',
+        hair: 32,
+        address: {
+            city: 'Minsk',
+            house: 12
+        },
+        laptop: {
+            title:'Zenbook'
+        },
+        books: ['css', 'html','js']
+    }
+
+    const userCopy = updateBook(user,'js','ts')
+
+    expect(userCopy).not.toBe(user)
+    expect(userCopy.books[2]).toBe('ts')
+    expect(userCopy.books).not.toBe(user.books)
+    expect(user.books.length).toBe(3)
+})
+
+test('remove js book',()=>{
+    let user:UserWithBooks={
+        name: 'Dimych',
+        hair: 32,
+        address: {
+            city: 'Minsk',
+            house: 12
+        },
+        laptop: {
+            title:'Zenbook'
+        },
+        books: ['css', 'html','js']
+    }
+
+    const userCopy = removeBook(user,'js')
+
+    expect(userCopy).not.toBe(user)
+    expect(userCopy.books).not.toBe(user.books)
+    expect(userCopy.books.length).toBe(2)
 })
--- src/10-immutability/10_01.ts
+++ src/10-immutability/10_01.ts
@@ -1,5 +1,71 @@
 export type UserType = {
-    name:string
+    name: string
     hair: number
-    address: {title:string}
+    address: {
+        city: string,
+        house: number
+    }
 }
+export type LaptopType = {
+    title: string
+}
+export type UserWithLaptopType = UserType & {
+    laptop: LaptopType
+}
+
+export type UserWithBooks = UserWithLaptopType & {
+    books: string[]
+}
+
+export function makeHairStyle(u: UserType, power: number) {
+    const copy = {
+        ...u,
+        hair: u.hair / power
+    }
+    // copy.hair = u.hair/power
+
+    return copy
+}
+
+export function moveUser(u: UserWithLaptopType, city: string) {
+    return {
+        ...u,
+        address: {
+            ...u.address,
+            city: city
+        }
+    }
+}
+
+export function upgradeUser(u: UserWithLaptopType, laptopName: string) {
+    return {
+        ...u,
+        laptop: {
+            ...u.laptop,
+            title: laptopName
+        }
+    }
+}
+
+export function addBooksToUser(u: UserWithBooks, newBook: string) {
+    const copy = {
+        ...u,
+        // books: {
+        // books: [...u.books]
+        // }
+        books: [...u.books]
+    }
+    copy.books.push(newBook)
+    return copy
+}
+
+export const updateBook = (u: UserWithBooks, oldBook: string, changeBook: string)=>({
+        ...u,
+        books: u.books.map(b => b === oldBook ? changeBook : b)
+    })
+
+export const removeBook = (u: UserWithBooks, removeThisBook: string)=> ({
+    ...u,
+        books: u.books.filter(b => b !== removeThisBook)
+
+})
